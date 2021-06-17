bd5f627cba3294e7d778014b501ffe4b02f38487
zhunor <ozerorhun@gmail.com>
2021-06-18 02:34:26 +0300

    WIP

--- pages/[lenderName].tsx
+++ pages/[lenderName].tsx
@@ -144,7 +144,14 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch(
     `${server}/api/lenders/${
       context.params?.lenderName ? context.params?.lenderName : ''
-    }`,
+    }`,{
+      headers: {
+        // update with your user-agent
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    }
   );
 
   const lenderData = await res.json();
